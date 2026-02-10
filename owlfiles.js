29f8a189f1895ac2e950f1845b07df2af45861d1
TANG <tangjyunsiao@gmail.com>
2026-02-10 21:07:12 +0800

    猫头鹰文件

--- owlfiles.js
+++ owlfiles.js
@@ -1,4 +1,4 @@
 var obj = JSON.parse($response.body);
-obj.data["memberLevel"] = 1
-obj.data["expireAt"] = 1773147720
+obj["memberLevel"] = 1
+obj["expireAt"] = 1773147720
 $done({body: JSON.stringify(obj)});
